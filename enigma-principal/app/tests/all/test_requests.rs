bd173d5aeb78373592f99ebd2004898072153a92
Elichai Turkel <elichai@enigma.co>
2019-05-12 16:22:45 +0300

    TST: Updated the integration test

--- enigma-principal/app/tests/all/test_requests.rs
+++ enigma-principal/app/tests/all/test_requests.rs
@@ -5,8 +5,8 @@ use enigma_principal_app::{
 };
 use enigma_tools_m::primitives::km_primitives::{PrincipalMessage, PrincipalMessageType};
 use jsonrpc_client_core::Error;
-use jsonrpc_client_http::HttpTransport;
-use rustc_hex::{ToHex, FromHex};
+use jsonrpc_client_http::{HttpTransport, HttpHandle};
+use rustc_hex::ToHex;
 use std::{
     thread::{self, JoinHandle},
     time,
@@ -19,26 +19,77 @@ pub struct Response {
     pub sig: String,
 }
 
-#[allow(dead_code)]
-fn generate_opts() -> Opt {
-    let args = ["test", "--mine", "5"];
-    let clap = Opt::clap().get_matches_from(&args);
-    Opt::from_clap(&clap)
-}
-
-#[allow(dead_code)]
-pub fn run_principal(opt: Opt) -> JoinHandle<()> {
-    thread::spawn(move || {
-        let enclave = esgx::general::init_enclave_wrapper().expect("[-] Init Enclave Failed");
-        let eid = enclave.geteid();
-        cli::app::start(eid, opt).unwrap();
-    })
-}
-
 jsonrpc_client!(pub struct KeyManagementClient {
-    /// Returns the fizz-buzz string for the given number.
-    pub fn getStateKeys(&mut self, data: String, sig: String) -> RpcRequest<Response>;
+    pub fn getStateKeys(&mut self, data: &str, sig: &str) -> RpcRequest<Response>;
 });
+
+
+
+#[test]
+fn test_bad_hex() {
+    let mut client = get_client(3040);
+
+    let res = client.getStateKeys("garbage", "More garbage").call();
+    println!("{:?}", res);
+}
+
+#[test]
+fn test_bad_sig() {
+    let mut client = get_client(3040);
+    let (msg, _) = generate_msg();
+    let sig = [255; 65];
+    let res = client.getStateKeys(&msg.to_hex(), &sig.to_hex()).call();
+    println!("{:?}", res);
+}
+
+#[test]
+fn test_zero_sig() {
+    let mut client = get_client(3040);
+    let (msg, _) = generate_msg();
+    let sig = [0; 65];
+    let res = client.getStateKeys(&msg.to_hex(), &sig.to_hex()).call();
+    println!("{:?}", res);
+}
+
+#[test]
+fn test_bad_sig_len() {
+    let mut client = get_client(3040);
+    let (msg, _) = generate_msg();
+    let sig = [255; 32];
+    let res = client.getStateKeys(&msg.to_hex(), &sig.to_hex()).call();
+    println!("{:?}", res);
+}
+
+#[test]
+fn test_bad_msg() {
+    let mut client = get_client(3040);
+    let (mut msg, sig) = generate_msg();
+    msg[10..20].copy_from_slice(&[255; 10]);
+    let res = client.getStateKeys(&msg.to_hex(), &sig.to_hex()).call();
+    println!("{:?}", res);
+}
+
+
+
+#[test]
+#[ignore]
+fn test_spam_success() {
+//    let opts = generate_opts();
+//    let _principal_handle =  run_principal(opts);
+    thread::sleep(time::Duration::from_secs(5));
+    let handles = send_requests(2000);
+    let handles = handles.into_iter().map(|h| h.join().unwrap()).collect::<Vec<_>>();
+    let failed_amounts = handles.iter().fold(0, |amount, result| {
+        if result.is_err() {
+            println!("{:?}", result);
+            amount + 1
+        } else {
+            amount
+        }
+    });
+    println!("failed: {} times", failed_amounts);
+//    principal_handle.join().unwrap();
+}
 
 fn sign_whatever(msg: &[u8]) -> [u8; 65] {
     let keys = KeyPair::new().unwrap();
@@ -58,31 +109,6 @@ fn generate_msg() -> (Vec<u8>, [u8; 65]) {
     (msg, sig)
 }
 
-#[test]
-fn first_test() {
-    //    let opts = generate_opts();
-    //    let principal_handle =  run_principal(opts);
-    thread::sleep(time::Duration::from_secs(5));
-    let handles = send_requests(2000);
-    let handles = handles.into_iter().map(|h| h.join().unwrap()).collect::<Vec<_>>();
-    let failed_amounts = handles.iter().fold(0, |amount, result| {
-        if result.is_err() {
-            println!("{:?}", result);
-            amount + 1
-        } else {
-            amount
-        }
-    });
-    let handles = handles.into_iter().filter_map(|h|
-        if let Ok(h) = h { Some(h) } else { None }
-    ).collect::<Vec<_>>();
-    println!("Failed {} times", failed_amounts);
-    let data1 = handles[0].data.from_hex().unwrap();
-    let km = PrincipalMessage::from_message(&data1).unwrap();
-    println!("{:?}", km);
-    //    principal_handle.join().unwrap();
-}
-
 fn send_requests(n: usize) -> Vec<JoinHandle<Result<Response, Error>>> {
     let mut handles = Vec::with_capacity(n);
     for i in 0..n {
@@ -90,13 +116,40 @@ fn send_requests(n: usize) -> Vec<JoinHandle<Result<Response, Error>>> {
             thread::sleep(time::Duration::from_millis(30));
         }
         let handle = thread::spawn(|| {
-            let transport = HttpTransport::new().standalone().unwrap();
-            let transport_handle = transport.handle("http://127.0.0.1:3040/").unwrap();
-            let mut client = KeyManagementClient::new(transport_handle);
+            let mut client = get_client(3040);
+
             let (msg, sig) = generate_msg();
-            client.getStateKeys(msg.to_hex(), sig.to_hex()).call()
+            client.getStateKeys(&msg.to_hex(), &sig.to_hex()).call()
         });
         handles.push(handle);
     }
     handles
+}
+
+
+fn get_client(port: u16) -> KeyManagementClient<HttpHandle> {
+    let transport = HttpTransport::new().standalone().unwrap();
+    let transport_handle = transport.handle(&format!("http://127.0.0.1:{}",port)).unwrap();
+
+    KeyManagementClient::new(transport_handle)
+}
+
+
+
+
+
+#[allow(dead_code)]
+fn generate_opts() -> Opt {
+    let args = ["test", "--mine", "5"];
+    let clap = Opt::clap().get_matches_from(&args);
+    Opt::from_clap(&clap)
+}
+
+#[allow(dead_code)]
+pub fn run_principal(opt: Opt) -> JoinHandle<()> {
+    thread::spawn(move || {
+        let enclave = esgx::general::init_enclave_wrapper().expect("[-] Init Enclave Failed");
+        let eid = enclave.geteid();
+        cli::app::start(eid, opt).unwrap();
+    })
 }
